ede6b8769ec5bb255e1c0129bd1f756538c001e1
LukasLapienis <lapienislukasp@gmail.com>
2024-03-08 01:16:59 +0200

    baigta

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const cors = require("cors");
 const connectingToDB = require('./config/db.js')
+const errorHandler = require("./middleware/errorHandler.js");
 
 connectingToDB()
 
