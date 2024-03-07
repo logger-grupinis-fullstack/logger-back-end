d3292d93dc85a5ddca82f5d9400e3ce746f5b43b
LukasLapienis <lapienislukasp@gmail.com>
2024-03-08 01:42:12 +0200

    added timestamp for user

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,23 +1,26 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    firstName:{
-        type: String,
-        required:[true, "Please add first name"],
+const userSchema = new mongoose.Schema(
+    {
+        firstName:{
+            type: String,
+            required:[true, "Please add first name"],
+        },
+        email:{
+            type: String,
+            unique: [true, "email already used"],
+            required:[true, "Please add email"],
+        },
+        password:{
+            type: String,
+            required:[true, "Please add password"],
+        },
+        role: {
+            type: String,
+            default: "simple",
+        },
     },
-    email:{
-        type: String,
-        unique: [true, "email already used"],
-        required:[true, "Please add email"],
-    },
-    password:{
-        type: String,
-        required:[true, "Please add password"],
-    },
-    role: {
-        type: String,
-        default: "simple",
-    }
-});
+    { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
