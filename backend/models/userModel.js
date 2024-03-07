3936954dee3b6373f9b70e90e934697ba70c04c5
KaLa <k.lapienis@gmail.com>
2024-03-07 13:56:49 +0200

    userModel added

---
+++ backend/models/userModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    firstName:{
+        type: String,
+        required:[true, "Please add first name"],
+    },
+    email:{
+        type: String,
+        unique: [true, "email already used"],
+        required:[true, "Please add email"],
+    },
+    password:{
+        type: String,
+        required:[true, "Please add password"],
+    },
+    role: {
+        type: String,
+        default: "simple",
+    }
+});
+
+module.exports = mongoose.model("User", userSchema);
