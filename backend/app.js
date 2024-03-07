8871ce3068ccc253a56c2b7f0ee2ea555b31b4fb
LukasLapienis <lapienislukasp@gmail.com>
2024-03-08 00:40:03 +0200

    created and connected to database

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,16 @@
-require("dotenv").config();
-const express = require("express");
-const connectToDB = require("./config/db.js");
+require('dotenv').config()
+const express = require('express')
 const cors = require("cors");
-const errorHandler = require("./middleware/errorHandler.js");
+const connectingToDB = require('./config/db.js')
 
-connectToDB();
+connectingToDB()
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.options("*", cors());
-app.use(express.urlencoded());
-app.use("/api/users", require("./routes/userRoutes.js"));
+app.options("*");
+app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/ads", require("./routes/adRoutes"));
-app.use(errorHandler);
-
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-});
-
+app.listen(process.env.PORT, () =>
+  console.log(`Server is running on port ${process.env.PORT}`)
+);
