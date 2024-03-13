1763f33408632f9807f8c3552489e01ddd728b55
unknown <kgirdziunas0@gmail.com>
2024-03-13 11:23:09 +0200

    user controllerio handlingas?

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -61,19 +61,25 @@ const registerUser = asyncHandler(async(req, res) => {
 const loginUser = asyncHandler(async(req, res) => {
     const {email, password} = req.body;
     const user = await User.findOne({email});
-    if(user && (await bcrypt.compare(password, user.password))) {
-        res.json({
-            _id: user.id,
-            firstName: user.firstName,
-            email: user.email,
-            token: generateToken(user._id)
-        });
-    }
-    else {
-        res.status(400)
-        throw new Error("Invalid credencials");
-    };
-});
+
+   if(!user){
+    res.status(404);
+    throw new Error('User not found')
+   }
+
+   const isPasswordValid = await bcrypt.compare(password, user.password)
+   if(!isPasswordValid) {
+    res.status(400);
+    throw new Error("Invalid credentials")
+   }
+
+   res.json({
+    _id: user.id,
+    firstName: user.firstName,
+    email: user.email,
+    token: generateToken(user._id)
+   })
+})
 
 
 module.exports = {registerUser, loginUser};
