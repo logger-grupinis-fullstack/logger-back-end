58d801f42f55f6e01d4c3dbbfeddeaa56242257d
Laurynasjoksas88 <144778600+Laurynasjoksas88@users.noreply.github.com>
2024-03-14 21:35:32 +0200

    Update userController.js
    JSON formatu gražina erora, bei validacija gražinama JSON tipu.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,28 +6,39 @@ const asyncHandler = require("express-async-handler")
 const registerUser = asyncHandler(async (req, res) => {
     const { firstName, email, password } = req.body;
 
-    const user = await userService.registerUser(firstName, email, password)
-    res.status(201).json(user);
+    try {
+        const user = await userService.registerUser(firstName, email, password);
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 });
 
 // user login
 
 const loginUser = asyncHandler(async (req, res) => {
-    const {email, password} = req.body;
+    const { email, password } = req.body;
 
-    const user = await userService.loginUser(email, password)
-    res.json(user)
-})
+    try {
+        const user = await userService.loginUser(email, password);
+        res.json(user);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
 
 // user logout
 
-const logoutUser = asyncHandler(async(req, res) => {
+const logoutUser = asyncHandler(async (req, res) => {
     const userId = req.user._id;
 
-    const result = await userService.logoutUser(userId);
-
-    res.status(200).json(result)
-})
+    try {
+        const result = await userService.logoutUser(userId);
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
 
 
 module.exports = { registerUser, loginUser, logoutUser }
