16706af8c16bb2b7fd7c2f6db131dd01a97ea529
kestulka <kgirdiunas0@gmail.com>
2024-03-19 09:57:15 +0200

    user routes, controller fixes

--- backend/routes/workTypeRoutes.js
+++ backend/routes/workTypeRoutes.js
@@ -3,18 +3,13 @@ const router = express.Router();
 
 const {
   createWorkType,
-  getWorkTypes,
-  getAllWorkTypes,
   updateWorkType,
   deleteWorkType,
 } = require("../controllers/workTypeController.js");
 const protect = require("../middleware/authMiddleware.js");
 
-// router.get("/:id/all", protect, getWorkTypes);
 router.put("/:id", protect, updateWorkType);
 router.delete("/:id", protect, deleteWorkType);
-
-// router.get("/all", protect, getAllWorkTypes);
 router.post("/", protect, createWorkType);
 
 module.exports = router;
